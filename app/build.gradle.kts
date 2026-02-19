feb4c81e138b7c3dd8ab86dd5cf43b95e9200e4b
saraebadi <ebadi.sara227@gmail.com>
2026-02-19 22:03:40 +0330

    Upgrade to AGP 9.0.1 & dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,21 +1,21 @@
 plugins {
-    alias(libs.plugins.jetbrains.kotlin.android)
     alias(libs.plugins.android.application)
+    alias(libs.plugins.kotlin.android)
     alias(libs.plugins.compose.compiler)
     alias(libs.plugins.secrets.gradle)
-    alias(libs.plugins.kotlin.kapt)
     alias(libs.plugins.hilt)
+    alias(libs.plugins.kotlin.ksp)
     alias(libs.plugins.kotlin.serialization)
 }
 
 android {
     namespace = "com.saraebadi.compose_coin"
-    compileSdk = 34
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "com.saraebadi.compose_coin"
         minSdk = 24
-        targetSdk = 34
+        targetSdk = 36
         versionCode = 1
         versionName = "1.0"
 
@@ -35,17 +35,12 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
     }
-    kotlinOptions {
-        jvmTarget = "1.8"
-    }
+
     buildFeatures {
         compose = true
-    }
-    composeOptions {
-        kotlinCompilerExtensionVersion = "1.5.1"
     }
     packaging {
         resources {
@@ -69,8 +64,7 @@ dependencies {
         implementation(androidx.ui.tooling.preview)
         implementation(androidx.lifecycle.runtime.ktx)
         implementation(platform(libs.androidx.compose.bom))
-
-        kapt(hilt.compiler)
+        ksp(hilt.compiler)
         implementation(coil)
         implementation(moshi)
         implementation(timber)
