4f264dc951cbd1323fdda990e07c5d1cd2b26f49
saraebadi <ebadi.sara227@gmail.com>
2026-02-25 16:52:47 +0330

    Refactor: Improve market list item UI.

--- app/src/main/java/com/saraebadi/compose_coin/presentation/marketlist/MarketListScreen.kt
+++ app/src/main/java/com/saraebadi/compose_coin/presentation/marketlist/MarketListScreen.kt
@@ -2,9 +2,9 @@ package com.saraebadi.compose_coin.presentation.marketlist
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
@@ -12,6 +12,7 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
@@ -34,27 +35,50 @@ fun MarketListScreen(
     onItemClicked: (Market) -> Unit
 ) {
     Box(modifier = modifier.fillMaxSize()) {
-        LazyColumn(modifier = Modifier.fillMaxSize()) {
-            items(marketListState.markets, key = { it.id }) {
+        LazyColumn(modifier = Modifier.fillMaxSize(),
+            contentPadding = PaddingValues(vertical = 8.dp)
+        ) {
+            items(marketListState.markets, key = { it.id }) { market ->
                 Row(
                     modifier = modifier
-                        .padding(16.dp)
+                        .padding(8.dp)
                         .fillMaxWidth()
-                        .background(Color.Gray, RoundedCornerShape(8.dp))
+                        .background(Color.LightGray, RoundedCornerShape(8.dp))
                         .padding(16.dp),
+                    verticalAlignment = Alignment.CenterVertically
                 ) {
-                    Image(
-                        painter = painterResource(R.drawable.ic_launcher_background),
-                        contentDescription = it.name,
-                        modifier = Modifier.size(24.dp),
-                        )
+                    AsyncImage(
+                        model = market.imageUrl,
+                        contentDescription = market.name,
+                        modifier = Modifier.size(24.dp)
+                    )
+
                     Spacer(modifier = Modifier.width(8.dp))
+
                     Column(
-                        horizontalAlignment = Alignment.CenterHorizontally,
-                        verticalArrangement = Arrangement.Center
+                        modifier = Modifier
+                            .wrapContentSize()
+                            .padding(start = 8.dp),
                     ) {
-                        Text(text = it.name)
-                        Text(text = it.name)
+                        Text(text = market.name)
+                        Text(modifier = Modifier.padding(top = 4.dp), text = market.symbol)
+                    }
+
+                    Spacer(modifier = Modifier.weight(1f))
+
+                    Column(
+                        modifier = Modifier.wrapContentSize()
+                            .padding(end = 4.dp),
+                        horizontalAlignment = Alignment.End
+                    ) {
+                        Text(text = "${market.currentPrice}")
+                        Text(
+                            modifier = Modifier
+                                .padding(top = 4.dp)
+                                .background(Color.Red, RoundedCornerShape(20.dp))
+                                .padding(vertical = 2.dp, horizontal = 12.dp),
+                            text = market.priceChangePercentage24h.toString()
+                        )
                     }
                 }
             }
@@ -66,12 +90,12 @@ fun MarketListScreen(
 @Composable
 private fun MarketListScreenPreview() {
     val list = listOf(
-        Market(id = "1", name = "zzzzzzzzzz"),
-        Market(id = "2", name = "xxxxxxxxx"),
-        Market(id = "3", name = "vvvvvvvvv"),
-        Market(id = "4", name = "aaaaaaaa"),
-        Market(id = "5", name = "wwwwwwww"),
-        Market(id = "6", name = "eeeeee"),
+        Market(id = "1", name = "zzzzzzzzzz", symbol = "btd", imageUrl = ""),
+        Market(id = "2", name = "xxxxxxxxx", symbol = "btd", imageUrl = ""),
+        Market(id = "3", name = "vvvvvvvvv", symbol = "btd", imageUrl = ""),
+        Market(id = "4", name = "aaaaaaaa", symbol = "btd", imageUrl = ""),
+        Market(id = "5", name = "wwwwwwww", symbol = "btd", imageUrl = ""),
+        Market(id = "6", name = "eeeeee", symbol = "btd", imageUrl = ""),
     )
     MarketListScreen(
         marketListState = MarketListState(markets = list),
